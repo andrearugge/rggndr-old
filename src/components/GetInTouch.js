280f2b4d43337766eb05acdd107701454117ec6f
Andrea Ruggeri <hello@rggndr.it>
2020-10-20 19:25:33 +0200

    Finish home

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,4 +1,6 @@
+import { Link } from "gatsby"
 import React from "react"
+import StickerLaugh from "../components/img/StickerLaugh"
 
 export default () => (
   <div className="section section-container s-getInTouch">
@@ -16,9 +18,12 @@ export default () => (
     </div>
 
     <div className="mega-title">
-      Wanna<br />
-      Get in<br />
-      Touch
+      <span>Wanna</span><br />
+      <span>Get in</span><br />
+      <span className="p-rel">
+        Touch?
+        <StickerLaugh />
+      </span>
     </div>
 
     <div className="row c-mt-6">
@@ -29,9 +34,10 @@ export default () => (
           <div>Social</div>
         </div>
         <div className="info-item">
-          <div>hello@rggndr.it</div>
+          <div><a href="mailto:hello@rggndr.it">hello@rggndr.it</a></div>
           <div>+39 393 8576 657</div>
-          <div>Instagram – Dribbble – LinkedIn</div>
+          <div>
+            <Link to="https://www.instagram.com/andrearugge/" target="_blank">Instagram</Link> – <Link to="https://dribbble.com/andrearugge" target="_blank">Dribbble</Link> – <Link to="https://www.linkedin.com/in/andrerugge/" target="_blank">LinkedIn</Link></div>
         </div>
       </div>
     </div>
--- src/components/img/StickerLaugh.js
+++ src/components/img/StickerLaugh.js
@@ -2,10 +2,10 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-const Logo = () => {
+const StickerLaugh = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "section-animal/animale-1.svg" }) {
+      placeholderImage: file(relativePath: { eq: "sticker-laugh.png" }) {
         childImageSharp {
           fluid(maxWidth: 700) {
             ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -15,7 +15,7 @@ const Logo = () => {
     }
   `)
 
-  return <Img className="image rounded" fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img className="sticker-laugh" fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
-export default Logo
+export default StickerLaugh
