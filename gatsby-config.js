50c0bd24595832963410add6910c1ce7e599f5fc
Andrea Ruggeri <hello@rggndr.it>
2021-01-12 22:31:26 +0100

    Remove GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,35 +5,35 @@ module.exports = {
     author: `Andrea Ruggeri <andrea@beconcept.studio> | Alessio Bortolotti <alessio@beconcept.studio>`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      // DOCS --> https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-google-analytics
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "'",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 100,
-        // Enables Google Optimize using your container Id
-        optimizeId: "",
-        // Enables Google Optimize Experiment ID
-        experimentId: "",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        siteSpeedSampleRate: 10,
-        cookieDomain: "",
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   // DOCS --> https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-google-analytics
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: "'",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //     // Setting this parameter is optional
+    //     anonymize: false,
+    //     // Setting this parameter is also optional
+    //     respectDNT: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 100,
+    //     // Enables Google Optimize using your container Id
+    //     optimizeId: "",
+    //     // Enables Google Optimize Experiment ID
+    //     experimentId: "",
+    //     // Set Variation ID. 0 for original 1,2,3....
+    //     variationId: "",
+    //     // Defers execution of google analytics script after page load
+    //     defer: false,
+    //     // Any additional optional fields
+    //     siteSpeedSampleRate: 10,
+    //     cookieDomain: "",
+    //   },
+    // },
     // {
     //   resolve: `gatsby-source-spotify`,
     //   options: {
